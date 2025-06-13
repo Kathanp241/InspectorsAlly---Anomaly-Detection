a28dd02306364c159ba84dccb2b3c6e4cc340d15
KATHAN PATEL <kathanp2401@gmail.com>
2025-06-13 19:56:59 +0530

    Update streamlitapp1.py

--- streamlitapp1.py
+++ streamlitapp1.py
@@ -85,7 +85,7 @@ class_names = open("labels.txt", "r").readlines()
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
 # determined by the first position in the shape tuple, in this case 1
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+data = np.ndarray(shape=(1, 224, 200, 3), dtype=np.float32)
 
 
 
